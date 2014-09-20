cfbd1edf544a1a11fff0715aa55e7b4a8c45cb9b
gr2301007 <gruiz16@student.rcc.edu>
2014-09-20 12:18:48 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/assignment2.s
+++ Homework/assignment2.s
@@ -1,10 +1,25 @@
 	.global _start
 _start:
 
-	mov r1, #111
-	mov r2, #5
+	mov r1, #111  /* a */
+	mov r2, #5    /* b */
+	mov r3, #0  /*counter*/
+
 	cmp r1, r2
-	bne zeroclear
-	
+	bne case_different
+	blt case_lower
+
+case_different:
+	sub r1, r1, r2
+	add r3, r3, #1
+	cmp r1, r2
+	blt case_lower
+	bne case_different
+
+case_lower:
+	mov r0, r3
+	b end
+
+end:
 	mov r7, #1
 	swi 0
