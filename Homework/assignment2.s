c99e0f574ce34f3246e6430cd8d8da3b144a9ce0
gr2301007 <gruiz16@student.rcc.edu>
2014-09-20 20:30:53 +0000

    test

--- Homework/assignment2.s
+++ Homework/assignment2.s
@@ -1,20 +1,28 @@
 	.global _start
 _start:
 
-	mov r1, #111  /* a */
+	mov r1, #100   /* a */
 	mov r2, #5    /* b */
 	mov r3, #0  /*counter*/
 
 	cmp r1, r2
-	bne case_different
 	blt case_lower
+	bne case_different
+	beq case_equal
 
 case_different:
 	sub r1, r1, r2
 	add r3, r3, #1
 	cmp r1, r2
+	beq case_equal
 	blt case_lower
 	bne case_different
+
+case_equal:
+	sub r1, r1, r2
+	add r3, r3, #1
+	mov r0, r3
+	b end
 
 case_lower:
 	mov r0, r3
