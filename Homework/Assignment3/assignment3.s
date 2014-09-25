004ea9a315fc74bea9ac6e24576b99ce77823c15
gr2301007 <gruiz16@student.rcc.edu>
2014-09-25 13:05:15 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment3/assignment3.s
+++ Homework/Assignment3/assignment3.s
@@ -7,27 +7,36 @@
 .text
 .global main
 main:
-	mov r2, #111     /* a=111 */
-	mov r3, #5       /* b=5 */
-	mov r0, #0       /* r0=0 -> counter */
-	mov r1, r2       /* r1=a=111 */
-	mov r4, #0       /* if 1 swap r0 and r1 on exit else 0*/
+	mov r2, #2222  /* a */
+	mov r3, #5     /* b */
+	mov r4, #0     /*counter*/
+	mov r5, #0     /*flag not set*/
+	mov r6, #0     /*present scale 10^*/
+	mov r7, #0     /*scale factor= r3*r6 to subtract*/
+	mov r8, #10    /*shift factor 10*/
+	mov r9, #0     /*shift test r7*r8*/
 	
-	cmp  r1, r3      /* compare and leave if less */
-	blt  swap
-	/* Now just perform repeated subtractions */
-repeat_subtraction:
-	add r0, r0, #1   /* increment r0 */
-	sub r1, r1, r3   /* r1=r1-r3 */
-	cmp r1, r3       /* Note: subs won't work above need cmp!!! */
-	bge repeat_subtraction       /* repeat the subtraction */
+	cmp r2, r3
+	blt output
+	
+repeat:
+	sub r2, r2, r3
+	add r4, r4, #1
+	cmp r2, r3
+	bge repeat
+	
+output:
+	mov r0, r4
+	mov r1, r2
+	cmp r5, #0
+	bne swap
+	b end
+
 swap:
-	/* Exchange r0 and r1 using r5 with a swap if flag set */
-	cmp r4, #0
-	beq end;
-	mov r5, r0
-	mov r0, r1
-	mov r1, r5
+	mov r0, r2
+	mov r1, r4
+	b end
+
 end:
-	bx lr
-	
+	mov r7, #1
+	swi 0
