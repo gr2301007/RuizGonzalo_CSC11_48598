286a5f96b92d4a3a36f6a698c939d58a91649f7b
gr2301007 <gruiz16@student.rcc.edu>
2014-10-11 16:47:32 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment4/assignment4.s
+++ Homework/Assignment4/assignment4.s
@@ -3,17 +3,59 @@
 	10/13/14
 	Assignment 4
 */
+
+.data
  
+/* First message */
+.balign 4
+message1: .asciz "Enter numerator and denominator: "
+ 
+/* Second message */
+.balign 4
+message2: .asciz "%d / %d is %d\n"
+
+/* Third message */
+.balign 4
+message3: .asciz "%d % %d is %d\n"
+ 
+/* Format pattern for scanf */
+.balign 4
+scan_pattern : .asciz "%d %d"
+ 
+/* Where scanf will store the numerator */
+.balign 4
+number_read: .word 0
+
+/* Where scanf will store the denominator */
+.balign 4
+number_read2: .word 0
+ 
+.balign 4
+return: .word 0
+ 
+
 .text
 .global main
 main:
-	mov r2, #24    /* a */
-	mov r3, #6     /* b */
+	ldr r1, address_of_return        /* r1 ? &address_of_return */
+    	str lr, [r1]                     /* *r1 ? lr */
+ 
+    	ldr r0, address_of_message1      /* r0 ? &message1 */
+    	bl printf                        /* call to printf */
+
+	ldr r0, address_of_scan_pattern  /* r0 ? &scan_pattern */
+    	ldr r1, address_of_number_read   /* r1 ? &number_read */
+	ldr r2, address_of_number_read2
+    	bl scanf  
+	
+	ldr r2, address_of_number_read    /* a */
+	ldr r2, [r2]
+	ldr r3, address_of_number_read2
+	ldr r3, [r3]                      /* b */
 	mov r4, #1     /*scale factor*/
 	mov r5, r3     /*subtraction factor*/
 	mov r0, #0     /*counter*/
 	mov r1, r2     /*remainder*/
-	mov r6, #0     /*flag not set*/
 	
 	cmp r2, r3
 	blt output
@@ -41,15 +83,39 @@ scale_right:
 	bgt add_sub
 	
 output:
-	cmp r6, #0
-	bne swap
-	b end
+	mov r3, r0                       /* r3 ? r0 */
+    	ldr r1, address_of_number_read   /* r1 ? &number_read */
+    	ldr r1, [r1]                     /* r1 ? *r1 */
+	mov r4, r2
+	ldr r2, address_of_number_read2   /* r2 ? &number_read2 */
+    	ldr r2, [r2]
+    	ldr r0, address_of_message2      /* r0 ? &message2 */
+    	bl printf 
 
-swap:
-	mov r7, r0
-	mov r0, r1
-	mov r1, r7
-	b end
+	mov r3, r4                       /* r3 ? r0 */
+    	ldr r1, address_of_number_read   /* r1 ? &number_read */
+    	ldr r1, [r1]                     /* r1 ? *r1 */
+	mov r4, r2
+	ldr r2, address_of_number_read2   /* r1 ? &number_read */
+    	ldr r2, [r2]
+    	ldr r0, address_of_message3      /* r0 ? &message3 */
+    	bl printf 
+	
 
 end:
+	ldr lr, address_of_return        /* lr ? &address_of_return */
+    	ldr lr, [lr]                     /* lr ? *lr */
 	bx lr
+
+address_of_message1 : .word message1
+address_of_message2 : .word message2
+address_of_message3 : .word message3
+address_of_scan_pattern : .word scan_pattern
+address_of_number_read : .word number_read
+address_of_number_read2 : .word number_read2
+address_of_return : .word return
+ 
+/* External */
+.global printf
+.global scanf
+
