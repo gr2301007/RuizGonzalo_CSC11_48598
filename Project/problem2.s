c234e3c4dbb61fdd7b819842fe3fb4258c00b10d
gr2301007 <gruiz16@student.rcc.edu>
2014-10-25 12:21:03 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/problem2.s
+++ Project/problem2.s
@@ -1,13 +1,12 @@
 
-/* Problem 1*/
+/* Problem 2*/
 
 .data 
    
-message1: .asciz "\nIn problem 1\n\n"
-message2: .asciz "Enter hours worked and rate: "
-message3: .asciz "Gross payment is: %d\n\n"
-message4: .asciz "No more than 60 hours.\n\n"
-format:   .asciz "%d %d" 
+message1: .asciz "\nIn problem 2\n\n"
+message2: .asciz "Enter package(a,b,c) and hours: "
+message3: .asciz "Monthly bill is: %d\n\n"
+format:   .asciz "%c %d" 
   
 .text 
 
@@ -25,55 +24,17 @@ main:
      bl printf                    /* Call printf */
    
      ldr r0, address_of_format    /* Set format as the first parameter of scanf */ 
-     mov r2, sp                   /* Set variable of the stack as rate */ 
-     add r1, r2, #4               /* and second value as hours of scanf */ 
+     mov r2, sp                   /* Set variable of the stack as hours */ 
+     add r1, r2, #4               /* and second value as package of scanf */ 
      bl scanf                     /* Call scanf */ 
    
      add r1, sp, #4               /* Place sp+4 -> r1 */ 
-     ldr r1, [r1]                 /* Load the integer hours read by scanf into r1 */ 
-     ldr r2, [sp]		  /* Load the integer rate read by scanf into r2 */ 
+     ldr r1, [r1]                 /* Load the character package read by scanf into r1 */ 
+     ldr r2, [sp]		  /* Load the integer hours read by scanf into r2 */ 
      
-     cmp r1, #60
-     bgt invalid_hours
-
-     cmp r1, #20
-     ble first
-     bgt second
-
      
-
-     first:
-	mul r3, r1, r2
-	b output
-     
-     second:
-	cmp r1, #40
-        bge third
-
-	mov r4, #20
-	mul r3, r2, r4
-	sub r1, r1, #20
-	mov r5, #2
-	mul r6, r1, r5
-	mul r7, r2, r6
-	add r3, r3, r7
-	b output
-
-     third:
-	mov r4, #20
- 	mul r3, r2, r4
-	mov r5, #2
-	mul r6, r4, r5
-	mul r7, r2, r6
-	add r3, r3, r7
-	sub r1, r1, #40
-	mov r8, #3
-	mul r9, r8, r1
-	mul r10, r2, r9
-	add r3, r3, r10
 	
      output:
-     mov r1,r3
      ldr r0, address_of_message3  /* Set &message3 as the first parameter of printf */ 
      bl printf                    /* Call printf */
      b end
@@ -90,5 +51,4 @@ main:
 address_of_message1: .word message1 
 address_of_message2: .word message2 
 address_of_message3: .word message3
-address_of_message4: .word message4
 address_of_format:   .word format 
