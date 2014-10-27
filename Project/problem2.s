29ada38ea3319e37391d0ba889a0d8746b37f95c
gr2301007 <gruiz16@student.rcc.edu>
2014-10-26 21:07:35 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/problem2.s
+++ Project/problem2.s
@@ -9,7 +9,7 @@
 message1: .asciz "\nIn problem 2\n\n"
 message2: .asciz "Enter package(a,b,c) and hours: "
 message3: .asciz "Monthly bill is: %d\n\n"
-format:   .asciz "%d %c"
+format:   .asciz "%c %d"
   
 .text 
 
@@ -32,8 +32,8 @@ problem2:
      bl scanf                     /* Call scanf */ 
    
      add r1, sp, #4               /* Place sp+4 -> r1 */ 
-     ldr r1, [sp]                 /* Load the character package read by scanf into r1 */ 
-     ldr r2, [r1]		  /* Load the integer hours read by scanf into r2 */ 
+     ldr r1, [r1]                 /* Load the character package read by scanf into r1 */ 
+     ldr r2, [sp]		  /* Load the integer hours read by scanf into r2 */ 
      
      cmp r1, #97		/* 97  = a */
      beq pckga
@@ -143,7 +143,7 @@ problem2:
      end:
      add sp, sp, #8               /* Discard the integer read by scanf */ 
      ldr lr, [sp], #+4            /* Pop the top of the stack and put it in lr */ 
-     bx lr                        /* Leave main */
+     bx lr                        /* problem2 */
    
 address_of_message1: .word message1 
 address_of_message2: .word message2 
