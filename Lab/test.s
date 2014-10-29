644a607a5b8dd08b3d8b2b24cc1418aed06d5a6a
gr2301007 <gruiz16@student.rcc.edu>
2014-10-28 18:00:11 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

---
+++ Lab/test.s
@@ -0,0 +1,30 @@
+
+/* 
+   Gonzalo Ruiz
+   project test */
+
+.data 
+   
+message1: .asciz " | %c | %c | %c "
+
+
+ .text 
+
+
+.global main
+
+main: 
+
+     mov r1, #97
+     mov r2, #98
+     mov r3, #99
+     ldr r0, address_of_message1  /* Set &message1 as the first parameter of printf */ 
+     bl printf                    /* Call printf */ 
+
+     bx lr                        /* Leave main */ 
+	
+     
+   
+address_of_message1: .word message1 
+address_of_message2: .word message2 
+address_of_format:   .word format 
