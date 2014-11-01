f8267dc81efa0ebe8cc674a63eb62eade57517e1
gr2301007 <gruiz16@student.rcc.edu>
2014-10-31 21:18:37 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/test.s
+++ Project/test.s
@@ -5,10 +5,9 @@
 
 
 
-/* Assignment 5*/
-
 .data 
-   
+
+newline: .asciz "\n   
 message1: .asciz "%c " 
 format:   .asciz "%c" 
 
@@ -22,35 +21,83 @@ main:
     str lr, [sp,#-4]!            /* Push lr onto the top of the stack */ 
     sub sp, sp, #4               /* Make room for one 4 byte integer in the stack */ 
 
+    mov r2, #95
+    mov r3, #95
+    mov r4, #95
+    mov r5, #95
+    mov r6, #95
+    mov r7, #5			 /*length of the word*/
+
+    loop:
     ldr r0, address_of_format    /* Set &format as the first parameter of scanf */
     mov r1, sp                   /* Set the top of the stack as the second parameter */
                                  /* of scanf */
     bl scanf                     /* Call scanf */
-    ldr r1, [sp]		 /* Load integer read into r1*/
-     
-    mov r1, #67
+    ldr r1, [sp]		 /* Load character read into r1*/
+
+    cmp r1, #67
+    beq letter_c
+
+    cmp r1, #104
+    beq letter_h
+
+    cmp r1, #105
+    beq letter_i
+
+    cmp r1, #110
+    beq letter_n
+
+    cmp r1, #97
+    beq letter_a
+
+    letter_c:
+	mov r2, r1
+	b output
+    letter_h:
+	mov r3, r1
+	b output
+    letter_i:
+	mov r4, r1
+	b output
+    letter_n:
+	mov r5, r1
+	b output
+    letter_a:
+	mov r6, r1
+	b output
+
+    output: 
+    mov r1, r2
     ldr r0, address_of_message1  /* Set &message1 as the first parameter of printf */ 
     bl printf                    /* Call printf */ 
 
-    mov r1, #104
+    mov r1, r3
     ldr r0, address_of_message1
     bl printf 
 
-    mov r1, #105
+    mov r1, r4
     ldr r0, address_of_message1
     bl printf 
 
-    mov r1, #110
+    mov r1, r5
     ldr r0, address_of_message1
     bl printf 
 
-    mov r1, #97
+    mov r1, r6
     ldr r0, address_of_message1
     bl printf 
+
+    ldr r0, adress_of_newline
+    bl printf
+
+    sub r7, r7, #1
+    cmp r7, #0
+    bne loop
     
      add sp, sp, #4              /* Discard the integer read by scanf */     
      ldr lr, [sp], #+4            /* Pop the top of the stack and put it in lr */ 
      bx lr                        /* Leave main */ 
    
+address_of_newline: .word newline
 address_of_message1: .word message1 
 address_of_format:   .word format
