ea3503105913e9bf5ee18bdcf2f728ade016ddcc
gr2301007 <gruiz16@student.rcc.edu>
2014-10-31 20:07:42 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/test.s
+++ Project/test.s
@@ -20,16 +20,19 @@ format:   .asciz "%c"
 
 main: 
     str lr, [sp,#-4]!            /* Push lr onto the top of the stack */ 
+    sub sp, sp, #4               /* Make room for one 4 byte integer in the stack */ 
 
     ldr r0, address_of_format    /* Set &format as the first parameter of scanf */
     mov r1, sp                   /* Set the top of the stack as the second parameter */
                                  /* of scanf */
     bl scanf                     /* Call scanf */
+    ldr r1, [sp]		 /* Load integer read into r1*/
      
     ldr r0, address_of_message1  /* Set &message1 as the first parameter of printf */ 
      bl printf                    /* Call printf */ 
    
     
+     add sp, sp, #4              /* Discard the integer read by scanf */     
      ldr lr, [sp], #+4            /* Pop the top of the stack and put it in lr */ 
      bx lr                        /* Leave main */ 
    
