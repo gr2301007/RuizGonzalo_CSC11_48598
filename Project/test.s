7c5e8708f9ffce5ac1c5712b34994f6b3cd8c7f5
gr2301007 <gruiz16@student.rcc.edu>
2014-10-31 19:58:38 -0700

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/test.s
+++ Project/test.s
@@ -9,7 +9,8 @@
 
 .data 
    
-message1: .asciz "Hello %c bye\n" 
+message1: .asciz "%d\n" 
+format:   .asciz "%c" 
 
   
 .text 
@@ -18,11 +19,14 @@ message1: .asciz "Hello %c bye\n"
 .globl main 
 
 main: 
-     str lr, [sp,#-4]!            /* Push lr onto the top of the stack */ 
+    str lr, [sp,#-4]!            /* Push lr onto the top of the stack */ 
+
+    ldr r0, address_of_format    /* Set &format as the first parameter of scanf */
+    mov r1, sp                   /* Set the top of the stack as the second parameter */
+                                 /* of scanf */
+    bl scanf                     /* Call scanf */
      
- 	 
-     mov r1, #97
-     ldr r0, address_of_message1  /* Set &message1 as the first parameter of printf */ 
+    ldr r0, address_of_message1  /* Set &message1 as the first parameter of printf */ 
      bl printf                    /* Call printf */ 
    
     
@@ -30,4 +34,4 @@ main:
      bx lr                        /* Leave main */ 
    
 address_of_message1: .word message1 
-
+address_of_format:   .word format
