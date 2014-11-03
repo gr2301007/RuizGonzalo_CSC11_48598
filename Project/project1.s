850f7217a28c56e025cff2c10122f1ccc4bff819
gr2301007 <gruiz16@student.rcc.edu>
2014-11-02 17:05:02 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/project1.s
+++ Project/project1.s
@@ -8,7 +8,13 @@ message1: .asciz "Welcome to hangman...Guess a country name\n"
 message2: .asciz "You have 5 tries to guess the word\n"
 message3: .asciz "Sorry you've been hanged\n"
 message4: .asciz "Congratulations you win!\n"
-format:   .asciz "%d " 
+message5: .asciz "The word was: "
+message6: .asciz "China\n"
+message7: .asciz "Spain\n"
+message8: .asciz "Iraq\n"
+message9: .asciz "Haiti\n"
+message10: .asciz "Cuba\n"
+
 
 
 .text 
@@ -16,8 +22,13 @@ format:   .asciz "%d "
 .globl main 
 
 main: 
-    str lr, [sp,#-4]!            /* Push lr onto the top of the stack */ 
-    sub sp, sp, #4               /* Make room for one 4 byte integer in the stack */
+    push {lr}
+
+    ldr r0, address_of_message1
+    bl printf 
+
+    ldr r0, address_of_message2
+    bl printf 
 
     mov r0, #0
     bl time
@@ -28,16 +39,73 @@ main:
     bl divMod
     add r1, #1
 
-    ldr r0, address_of_format
-    bl printf 
+    mov r1, #1
+
+    cmp r1, #1
+    beq w1
+    cmp r1, #2
+    beq w2
+    cmp r1, #3
+    beq w3
+    cmp r1, #4
+    beq w4
+    cmp r1, #5
+    beq w5
+
+    w1:
+    bl word1
+    b test
+
+    test:
+    cmp r0, #1
+    beq lose
     
+    ldr r0, address_of_message4
+    bl printf
+    b output
+
+    lose:
+    ldr r0, address_of_message3
+    bl printf
+
+    output:
+    dr r0, address_of_message5
+    bl printf 
+
+    cmp r1, #1
+    beq o1
+  
+    cmp r1, #2
+    beq o2
+   
+    cmp r1, #3
+    beq o3
+   
+    cmp r1, #4
+    beq o4
+   
+    cmp r1, #5
+    beq o5
+
+    o1:
+    ldr r0, address_of_message6
+    bl printf 
+    b end
+    
+    
+    
+
     end:
-     add sp, sp, #4              /* Discard the integer read by scanf */     
-     ldr lr, [sp], #+4           /* Pop the top of the stack and put it in lr */ 
-     bx lr                       /* Leave main */ 
+    pop {lr}             /* Leave main */ 
    
 address_of_message1: .word message1
 address_of_message2: .word message2 
 address_of_message3: .word message3
 address_of_message4: .word message4
-address_of_format:   .word format
+address_of_message5: .word message3
+address_of_message6: .word message4
+address_of_message7: .word message3
+address_of_message8: .word message4
+address_of_message9: .word message3
+address_of_message10: .word message4
+
