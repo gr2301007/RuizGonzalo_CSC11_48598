ffeb878a25d8980ba2854d62aaf9ffe29673b130
gr2301007 <gruiz16@student.rcc.edu>
2014-11-05 11:28:29 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/word3.s
+++ Project/word3.s
@@ -13,9 +13,9 @@ format:   .asciz " %c"
 
 .text 
 
-.global word3
+.global main
 
-word3: 
+main: 
     str lr, [sp,#-4]!            /* Push lr onto the top of the stack */ 
     sub sp, sp, #4               /* Make room for one 4 byte integer in the stack */
 
