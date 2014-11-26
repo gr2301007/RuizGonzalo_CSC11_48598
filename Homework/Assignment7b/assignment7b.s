e4756882f28195fd01ab15b7e8610031ad1b0946
gr2301007 <gruiz16@student.rcc.edu>
2014-11-25 18:09:04 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment7b/assignment7b.s
+++ Homework/Assignment7b/assignment7b.s
@@ -109,7 +109,7 @@ main:
      cmp r0, #212
      bgt invalid
 
-     mov r7, #4194304          	  /*Number of loops*/
+     ldr r7, =0x5f5e100           /*Number of loops*/
      mov r8, r0
 
      mov r0, #0 
