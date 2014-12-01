6342ed38a556aaf25bc60d91b0ca898215ee9b76
gr2301007 <gruiz16@student.rcc.edu>
2014-12-01 15:09:14 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment7c/test.s
+++ Homework/Assignment7c/test.s
@@ -10,12 +10,18 @@ message11: .asciz "\nCelsius(Float multiplication) = %f\n"
 
 .global main
 main:
-
+	mov r0 #100
+	sub r2 r0, #32
+	vldr s0, [r2]
+	
 	ldr r1, =value1
-	vldr s14, [r1]
-	vcvt.f64.f32 d5, s14
+	vldr s1, [r1]
+
+	vmul.f32 s2, s0, s1
+	vcvt.f64.f32 d2, s2
+	
 	ldr r0, =message11
-	vmov r2, r3, d5
+	vmov r2, r3, d2
 	bl printf
 
 	mov r7, #1
