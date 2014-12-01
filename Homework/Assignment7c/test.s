15cacb943c2be0e1f9a5c614ebc9933bf8796a75
gr2301007 <gruiz16@student.rcc.edu>
2014-12-01 14:56:31 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment7c/test.s
+++ Homework/Assignment7c/test.s
@@ -1,22 +1,22 @@
 .global main
 .func main
 
+.data
+
+value1: .float 0.55556
+message11: .asciz "\nCelsius(Float multiplication) = %f\n"
+
 main:
 
 	ldr r1, =value1
 	vldr s14, [r1]
 	vcvt.f64.f32 d5, s14
-	ldr r0, =string
+	ldr r0, =message11
 	vmov r2, r3, d5
 	bl printf
 
 	mov r7, #1
 	swi 0
 
-addr_value1:
-	.word value1
+addr_value1: .word value1
 
-	.data
-
-value1: .float 1.54321
-string: .asciz "Floating Point value is: %f\n"
