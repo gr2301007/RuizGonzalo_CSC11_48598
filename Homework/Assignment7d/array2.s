2aeee7f8070b98319792a2ee2bc62ffdc06f48b0
gr2301007 <gruiz16@student.rcc.edu>
2014-12-02 17:28:48 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment7d/array2.s
+++ Homework/Assignment7d/array2.s
@@ -4,6 +4,11 @@
  */
  
  .data 
+
+value1: .float 32
+value2: .float 5
+value3: .float 0.55556
+
    
  .align 4 
  array : 
@@ -14,7 +19,7 @@
  .skip 148 
    
  .align 4 
- message: .asciz "fahrenheit = %d, celsius(int) = %d\n" 
+ message: .asciz "fahrenheit = %d, celsius(int) = %d, " 
  message1: .asciz "float = %f\n" 
    
  .text 
@@ -52,12 +57,12 @@ float_array :
      push {r4, r5, r6, lr} 
    
      mov r4, #0      /* r4 ? 0 */ 
-     mov r5, #32
+     ldr r5, =value1
    
      b .Lcheck_loop_float_array 
      .Lloop_float_array: 
        str r5, [r1, r4, LSL #2]   /* *(r1 + r4 * 4) ? r5 */ 
-       add r5, r5, #5
+       
        add r4, r4, #1             /* r4 ? r4 + 1 */ 
      .Lcheck_loop_float_array: 
        cmp r4, r0                 /* r4 - r0 and update cpsr */ 
@@ -89,6 +94,15 @@ float_array :
        mov r1, r10      /* second parameter: item position */ 
        mov r2, r5      /* third parameter: item value */ 
        bl printf       /* call printf */ 
+
+       ldr r5, [r2, r4, LSL #2]
+       vldr s14, [r5]
+       vcvt.f64.f32 d5, s14
+
+       ldr r0, address_of_message1 /* first parameter of the call to printf below */ 
+	
+       vmov r2, r3, d5
+       bl printf       /* call printf */ 
    
        str r5, [r7, r4, LSL #2]   /* *(r7 + r4 * 4) ? r5 */ 
        add r4, r4, #1             /* r4 ? r4 + 1 */ 
@@ -109,6 +123,7 @@ float_array :
      /* second call print_each_item */ 
      mov r0, #37                   /* first_parameter: number of items */ 
      ldr r1, address_of_array   /* second parameter: address of the array */ 
+     ldr r2, address_of_float_array
      bl print_each_item             /* call to print_each_item */ 
    
      pop {r4, lr} 
