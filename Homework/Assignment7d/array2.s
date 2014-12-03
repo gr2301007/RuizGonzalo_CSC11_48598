3c6d2ec403b411cd4877a0099fee32b6934f1126
gr2301007 <gruiz16@student.rcc.edu>
2014-12-02 19:11:22 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment7d/array2.s
+++ Homework/Assignment7d/array2.s
@@ -68,7 +68,7 @@ float_array :
 
        vldr s14, [r5]
        vadd.f32 s14, s14, s15
-       vmov s14, s15, r5, r6
+       vmov.f32 r5, s14
        
        add r4, r4, #1             /* r4 ? r4 + 1 */ 
      .Lcheck_loop_float_array: 
