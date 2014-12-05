9b0bf7ad38e9e6454c270c983cbf0250f52ca249
gr2301007 <gruiz16@student.rcc.edu>
2014-12-05 11:05:11 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Homework/Assignment7d/array2.s
+++ Homework/Assignment7d/array2.s
@@ -10,10 +10,13 @@
  array : 
  .skip 152 
 
-
+.align 4 
+ f_array : 
+ .skip 152 
 
 .align 4 
-message: .asciz "fahrenheit = %d, celsius(int) = %d\n" 
+message: .asciz "fahrenheit = %d, celsius(int) = %d " 
+message1: .asciz "float = %f\n" 
 
 
 .text 
@@ -43,6 +46,35 @@ message: .asciz "fahrenheit = %d, celsius(int) = %d\n"
    
      bx lr 
 
+ float_array :  
+     /* Parameters:  
+            r0  Number of items 
+            r1  Address of the array 
+     */ 
+     push {r4, r5, r6, lr} 
+   
+     mov r4, #0      /* r4 ? 0 */ 
+     mov r5, #32
+   
+     b .Lcheck_loop_array 
+     .Lloop_array: 
+
+       vmov s0, r5
+       vcvt.f32.s32 s1,s0 
+       vmov r7, s1 
+       str r7, [r1, r4, LSL #2]
+
+       add r5, r5, #5
+
+      add r4, r4, #1             /* r4 ? r4 + 1 */ 
+      .Lcheck_loop_array: 
+       cmp r4, r0                 /* r4 - r0 and update cpsr */ 
+       bne .Lloop_array    /* if r4 != r0 go to .Lloop_array_double */ 
+   
+     pop {r4, r5, r6, lr} 
+   
+     bx lr 
+
 
 print_each_item: 
      push {r4, r5, r6, r7, r8, lr} /* r8 is unused */ 
@@ -50,6 +82,7 @@ print_each_item:
      mov r4, #0      /* r4 ? 0 */ 
      mov r6, r0      /* r6 ? r0. Keep r0 because we will overwrite it */ 
      mov r7, r1      /* r7 ? r1. Keep r1 because we will overwrite it */
+     mov r11, r2
      ldr r8, =0x8e38f 
    
    
@@ -67,6 +100,14 @@ print_each_item:
        mov r2, r5      /* third parameter: item value */ 
        bl printf       /* call printf */ 
 
+       ldr r5, [r11, r4, LSL #2]
+       vmov s14,r5 
+ 
+       vcvt.f64.f32 d5,s14 
+       vmov r2,r3,d5 
+       ldr r0, address_of_message1	
+       bl printf 
+
        add r4, r4, #1             /* r4 ? r4 + 1 */ 
      .Lcheck_loop_print_items: 
        cmp r4, r6                 /* r4 - r6 and update cpsr */ 
@@ -80,18 +121,25 @@ print_each_item:
      
      mov r0, #37                   /* first_parameter: number of items */ 
      ldr r1, address_of_array   /* second parameter: address of the array */ 
-     bl fill_array               /* call to double_array */ 
+     bl fill_array               /* call to double_array */
+
+     mov r0, #37                   /* first_parameter: number of items */ 
+     ldr r1, address_of_f_array   /* second parameter: address of the array */ 
+     bl float_array               /* call to double_array */  
 
      
      /* second call print_each_item */ 
      mov r0, #37                   /* first_parameter: number of items */ 
      ldr r1, address_of_array   /* second parameter: address of the array */
+     ldr r2, address_of_f_array   /* second parameter: address of the array */
      bl print_each_item             /* call to print_each_item */ 
    
      pop {r4, lr} 
      bx lr 
    
 address_of_array : .word array
+address_of_f_array : .word f_array
 address_of_message : .word message
+address_of_message1 : .word message1
 
 
