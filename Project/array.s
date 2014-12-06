763ac674c235b41082e30b8df14ca9b57f52a384
gr2301007 <gruiz16@student.rcc.edu>
2014-12-06 15:58:49 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/array.s
+++ Project/array.s
@@ -8,13 +8,12 @@
    
  .align 4 
  
-word1: 
- .word 6, 99, 104, 105, 110, 97
- 
- cover1:
- .word 6, 95, 95, 95, 95, 95
- .align 4 
-   
+word1: .word 6, 99, 104, 105, 110, 97
+cover1: .word 6, 95, 95, 95, 95, 95
+
+
+.align 4 
+
 message: .asciz "%c " 
 message2: .asciz "\n\nPick a letter: "
 message3: .asciz "\nThat letter isn't in the word\n"
@@ -34,6 +33,29 @@ replace_letter:
      ldr r6, [r0, r4, LSL #2] 
      mov r4, #1   
      mov r7, #0
+     mov r8, #0
+
+    b .Lcheck_letter 
+     .Lloop_repeat: 
+       ldr r5, [r1, r4, LSL #2]   /* r5 ? *(r7 + r4 * 4) */
+       
+	cmp r2, r5
+	beq used
+
+        b continue
+
+	used:
+           mov r8, #1  		/* flag letter already used*/
+	   mov r7, #1
+           b end
+       
+        continue:
+       add r4, r4, #1             /* r4 ? r4 + 1 */ 
+     .Lcheck_letter: 
+       cmp r4, r6                 /* r4 - r6 and update cpsr */ 
+       bne .Lloop_repeat       /* if r4 != r6 goto .Lloop_print_items */ 
+   
+   mov r4, #1
      
    b .Lcheck_loop_items 
      .Lloop_items: 
@@ -55,7 +77,9 @@ replace_letter:
        cmp r4, r6                 /* r4 - r6 and update cpsr */ 
        bne .Lloop_items       /* if r4 != r6 goto .Lloop_print_items */ 
 
+    end:
      mov r0, r7
+     mov r1, r8
    
      pop {r4, r5, r6, r7, r8, lr} 
      bx lr 
@@ -106,11 +130,6 @@ main:
      bl scanf                     /* Call scanf */
      ldr r11, [sp]		    
      
-     ldr r0, address_of_message 
-     mov r1, r11       
-
-     bl printf
-
      ldr r0, =word1
      ldr r1, =cover1
      mov r2, r11
@@ -118,13 +137,22 @@ main:
 
      cmp r0, #0
      beq not_found
+
+     cmp r1, #1
+     beq letter_used
+
      sub r4, r4, #1
 
      b output
 
      not_found:
         ldr r0, =message3
-        bl printf 
+        bl printf
+     b output
+
+     letter_used:
+        ldr r0, =message5
+        bl printf
 
      output:
      ldr r0, =cover1       /* first parameter: address of the array */
