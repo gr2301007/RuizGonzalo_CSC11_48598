016ad60f25105bd2f920b6a2b535dd92ff80394f
gr2301007 <gruiz16@student.rcc.edu>
2014-12-06 13:44:52 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/array.s
+++ Project/array.s
@@ -9,7 +9,7 @@
  .align 4 
  
  
- big_array : 
+ word1: 
  .word 6, 99, 104, 105, 110, 97
  
  .align 4 
@@ -59,5 +59,5 @@ main:
      pop {r4, lr} 
      bx lr 
    
- address_of_big_array : .word big_array 
+ address_of_word1: .word word1 
  address_of_message : .word message 
