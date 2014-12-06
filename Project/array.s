b06927f6c3b3ac1e8e693e6c6e35f3a1967bdeff
gr2301007 <gruiz16@student.rcc.edu>
2014-12-06 13:46:04 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Project/array.s
+++ Project/array.s
@@ -52,7 +52,7 @@ main:
      push {r4, lr} 
 
      
-     ldr r0, address_of_big_array   /* first parameter: address of the array */
+     ldr r0, address_of_word1   /* first parameter: address of the array */
      
      bl print_each_item             /* call to print_each_item */ 
    
