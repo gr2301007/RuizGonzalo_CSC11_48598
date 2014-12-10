8e8333f051c889d133d525a891a5cfb0f3062581
gr2301007 <gruiz16@student.rcc.edu>
2014-12-10 15:30:50 -0800

    Signed-off-by: gr2301007 <gruiz16@student.rcc.edu>

--- Lab/problem1.s
+++ Lab/problem1.s
@@ -90,6 +90,8 @@ main:
      mov r5, #10		  /*counter (guesses)*/
 
      mov r4, r1
+     ldr r0, =message3
+     bl printf
 
      ldr r0, =message2            /* Set &message2 as the first parameter of printf */ 
      bl printf                    /* Call printf */
